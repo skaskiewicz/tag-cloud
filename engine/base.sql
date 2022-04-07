28f42882f9a35636b14f475d894581212d973edb
Kamil <49995761+skaskiewicz@users.noreply.github.com>
2022-04-07 23:07:02 +0200

    template done 80% (for Version1) added fonteawesome update sql script (new tables)

--- engine/base.sql
+++ engine/base.sql
@@ -8,11 +8,20 @@ CREATE TABLE `tag-cloud`.tags (
     COLLATE=utf8mb4_general_ci;
 CREATE TABLE `tag-cloud`.main (
                                   id_main INT auto_increment NOT NULL,
-                                  id_tags INT NOT NULL,
                                   description VARCHAR(5000) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
                                   url varchar(5000) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-                                  CONSTRAINT id_main_pk PRIMARY KEY (id_main),
-                                  CONSTRAINT main_FK FOREIGN KEY (id_tags) REFERENCES `tag-cloud`.tags(id_tags)
+                                  CONSTRAINT id_main_pk PRIMARY KEY (id_main)
+)
+    ENGINE=InnoDB
+    DEFAULT CHARSET=utf8mb4
+    COLLATE=utf8mb4_general_ci;
+CREATE TABLE `tag-cloud`.tags_main (
+                                  id_tags_main INT auto_increment NOT NULL,
+                                  id_main INT NOT NULL,
+                                  id_tags INT NOT NULL,
+                                  CONSTRAINT id_tags_main_pk PRIMARY KEY (id_tags_main),
+                                  CONSTRAINT tags_main_FK FOREIGN KEY (id_main) REFERENCES `tag-cloud`.main(id_main),
+                                  CONSTRAINT tags_main_FK2 FOREIGN KEY (id_tags) REFERENCES `tag-cloud`.tags(id_tags)
 )
     ENGINE=InnoDB
     DEFAULT CHARSET=utf8mb4
