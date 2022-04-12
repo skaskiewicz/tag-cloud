3dceab542b466ca8e571750985f127353b505d4a
Kamil <49995761+skaskiewicz@users.noreply.github.com>
2022-04-12 22:53:29 +0200

    task-3 for now, the logic will be based on functions. i need to run favorites manager asap

---
+++ engine/env.php
@@ -0,0 +1,9 @@
+<?php
+$env_array = array(
+    "token" => "ThisIsNotSecretToken",
+    "db_host" => "192.168.0.24",
+    "db_port" => "3306",
+    "db_name" => "tag-cloud",
+    "db_user" => "tag-cloud",
+    "db_password" => "tag-cloud");
+?>
---
+++ engine/main.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once ('env.php');
+//function connect to database
+function connect($config)
+{
+    $db = new mysqli($config['db_host'], $config['db_user'], $config["db_password"], $config['db_name']);
+    if ($db->connect_errno) {
+        die("Failed to connect to MySQL: (" . $db->connect_errno . ") " . $db->connect_error);
+    }
+    return $db;
+}
+//function to get data from database
+$sql = "SELECT * FROM tags_main join main m on m.id_main = tags_main.id_main join tags t on t.id_tags = tags_main.id_tags";
+function get_data($db, $sql)
+{
+    $result = $db->query($sql);
+    if (!$result) {
+        die("Error: " . $db->error);
+    }
+    $data = array();
+    while ($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+    return $data;
+}?>
+<pre><?php
+print_r(get_data(connect($env_array), $sql));
+?>
+</pre>
