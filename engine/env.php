4bcb3b10ead9260a17e7591d274027a7b18c8068
Kamil <49995761+skaskiewicz@users.noreply.github.com>
2022-04-14 12:34:01 +0200

    task-3 reading from database adding from file to database viewing from database changing in database structure (one table but thinking to roll back to 3) colour array

--- engine/env.php
+++ engine/env.php
@@ -1,9 +1,37 @@
 <?php
+//configuration array
 $env_array = array(
     "token" => "ThisIsNotSecretToken",
     "db_host" => "192.168.0.24",
     "db_port" => "3306",
     "db_name" => "tag-cloud",
     "db_user" => "tag-cloud",
-    "db_password" => "tag-cloud");
+    "db_password" => "tag-cloud",
+    "db_prefix" => "");
+//colour array for randomised colour generation
+$colour_array = array(
+    0=>"w3-red",
+    1=>"w3-pink",
+    2=>"w3-blue",
+    3=>"w3-light-blue",
+    4=>"w3-cyan",
+    5=>"w3-aqua",
+    6=>"w3-teal",
+    7=>"w3-green",
+    8=>"w3-light-green",
+    9=>"w3-lime",
+    10=>"w3-sand",
+    11=>"w3-khaki",
+    12=>"w3-yellow",
+    13=>"w3-amber",
+    14=>"w3-orange",
+    15=>"w3-deep-orange",
+    16=>"w3-blue-grey",
+    17=>"w3-brown",
+    18=>"w3-light-grey",
+    19=>"w3-grey",
+    20=>"w3-pale-red",
+    21=>"w3-pale-green",
+    22=>"w3-pale-yellow",
+    23=>"w3-pale-blue");
 ?>
